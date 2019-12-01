b186d91eafaa3c1daad8b058e1ca7e3493b3b18a
Akshay R <46285388+appdevdude7@users.noreply.github.com>
2019-11-30 22:56:46 -0600

    added fetch dependency

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
-import Link from "next/link";
-export default () => (
-  <div>
-    Hello World.{" "}
-    <Link href="/about">
-      <a>About</a>
-    </Link>
-  </div>
-);
+// pages/index.js
+import Products from "../components/Products.js";
+import fetch from "isomorphic-unfetch";
+
+const Index = props => <Products products={props.products} />;
+
+Index.getInitialProps = async function() {
+  const res = await fetch(
+    "https://my-json-server.typicode.com/mood-board/demo/products"
+  );
+  const data = await res.json();
+  return {
+    products: data
+  };
+};
+export default Index;
